d90614a743beec832b39ab6114ae00833694299e
sarahelhawary <sarah.elhwarii92@gmail.com>
2020-12-31 19:37:23 +0200

    solve css errors

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// console.log('hello first')
+console.log('hello first')
 
-// function activeLinkFunction() {
-//   console.log('hello')
-//   const navLinks = document.querySelectorAll('.nav__link')
-//   navLinks.forEach((link) => {
-//     link.addEventListener('click', (event) => {
-//       const activeLink = document.querySelector('.nav__link.active')
-//       if (activeLink) {
-//         activeLink.classList.remove('active')
-//       }
-//       const currentLink = event.target
-//       console.log(currentLink)
-//       currentLink.classList.add('active')
-//       console.log(navLinks)
-//     })
-//   })
-// }
+function activeLinkFunction() {
+  console.log('hello')
+  const navLinks = document.querySelectorAll('.nav__link')
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      const activeLink = document.querySelector('.nav__link.active')
+      if (activeLink) {
+        activeLink.classList.remove('active')
+      }
+      const currentLink = event.target
+      console.log(currentLink)
+      currentLink.classList.add('active')
+      console.log(navLinks)
+    })
+  })
+}
 
-// activeLinkFunction()
+activeLinkFunction()
